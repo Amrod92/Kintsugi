7bee03425beb591225711ec78e3b6c0047b489d1
Manlio Chite <manlio92--@live.it>
2026-02-03 09:15:04 +0800

    Initial storie page implementation. Still need work to do

---
+++ app/storie/page.tsx
@@ -0,0 +1,33 @@
+export default function StoriePage() {
+  return (
+    <div>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-10 mx-auto">
+          <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+            Storie di Successo
+          </h1>
+
+          <p className="mt-4 text-gray-500 dark:text-gray-300">
+            Le nostre storie di successo riflettono l&apos;impegno e la
+            dedizione che mettiamo in ogni progetto. Ogni cliente rappresenta un
+            capitolo unico nel nostro viaggio, e siamo orgogliosi di condividere
+            alcuni dei nostri traguardi più significativi.
+          </p>
+
+          <p className="mt-4 text-gray-500 dark:text-gray-300">
+            Dalla collaborazione con piccole imprese locali alla realizzazione
+            di progetti su larga scala per aziende internazionali, ogni storia è
+            un testamento della nostra capacità di adattarci, innovare e
+            superare le aspettative.
+          </p>
+
+          <p className="mt-4 text-gray-500 dark:text-gray-300">
+            Siamo grati ai nostri clienti per la fiducia che ci hanno accordato,
+            e siamo entusiasti di continuare a costruire nuove storie di
+            successo insieme.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
